2f7aad9691a146b55b9663bc3906612da74b5a48
Nova <bluewallscrashing@gmail.com>
2022-07-27 17:26:26 -0700

    Comments Added

--- ScrumTheater/src/application/sceneController.java
+++ ScrumTheater/src/application/sceneController.java
@@ -15,7 +15,12 @@ public class sceneController
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
-	
+
+	/**
+	 * When the title is clicked, opens the home page.
+	 * @param event Mouse clicked on the label
+	 * @throws IOException
+	 */
 	public void titleClicked(MouseEvent event) throws IOException
 	{
 		root = FXMLLoader.load(getClass().getResource("homePage.fxml"));
@@ -24,6 +29,12 @@ public class sceneController
 		stage.setScene(scene);
 		stage.show();
 	}
+
+	/**
+	 * When home button is clicked, opens the home page.
+	 * @param event Button clicked
+	 * @throws IOException
+	 */
 	public void homeClicked(ActionEvent event) throws IOException
 	{
 		root = FXMLLoader.load(getClass().getResource("homePage.fxml"));
@@ -32,6 +43,12 @@ public class sceneController
 		stage.setScene(scene);
 		stage.show();
 	}
+
+	/**
+	 * When catalog button is clicked, opens the catalog page
+	 * @param event Button clicked
+	 * @throws IOException
+	 */
 	public void catalogClicked(ActionEvent event) throws IOException
 	{
 		root = FXMLLoader.load(getClass().getResource("catalogPage.fxml"));
@@ -40,8 +57,12 @@ public class sceneController
 		stage.setScene(scene);
 		stage.show();
 	}
-	
-	
+
+	/**
+	 * When tickets button is clicked, opens the tickets page.
+	 * @param event Button clicked
+	 * @throws IOException
+	 */
 	public void ticketsClicked(ActionEvent event) throws IOException
 	{
 		root = FXMLLoader.load(getClass().getResource("ticketPage.fxml"));
@@ -51,6 +72,11 @@ public class sceneController
 		stage.show();
 	}
 
+	/**
+	 * When account button is clicked, opens the Account page
+	 * @param event Button clicked
+	 * @throws IOException
+	 */
 	public void accountClicked(ActionEvent event) throws IOException
 	{
 		root = FXMLLoader.load(getClass().getResource("accountPage.fxml"));
@@ -59,14 +85,24 @@ public class sceneController
 		stage.setScene(scene);
 		stage.show();
 	}
-	
+
+	/**
+	 * When login button is clicked, open login page
+	 * @param event Button clicked
+	 * @throws IOException
+	 */
 	public void loginClicked(ActionEvent event) throws IOException
 	{
 		//No Login Yet
 			//Will log in the user using the username/email and the password
 			//Then, take them to the home page
 	}
-	
+
+	/**
+	 * When create account button is clicked, open the registration page.
+	 * @param event Button clicked
+	 * @throws IOException
+	 */
 	public void createAccClicked(ActionEvent event) throws IOException
 	{
 		root = FXMLLoader.load(getClass().getResource("registrationPage.fxml"));
